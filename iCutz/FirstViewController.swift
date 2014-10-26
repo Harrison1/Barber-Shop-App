0a0a0f1655af3e30171f791e5a13878e07cc9a2e
Harrison <hmcguire13@gmail.com>
2014-10-26 16:43:09 -0400

    Initial Commit

---
+++ iCutz/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iCutz
+//
+//  Created by Harrison McGuire on 10/26/14.
+//  Copyright (c) 2014 harrisonmcguire. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iCutz/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iCutz
+//
+//  Created by Harrison McGuire on 10/26/14.
+//  Copyright (c) 2014 harrisonmcguire. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
